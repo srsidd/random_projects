3d5d50ed4a9ee85723165c9c1540f15944766346
siddharth <srsidd@bitbucket.org>
2016-01-04 14:10:08 +0530

    added username display to the page

--- python/website.py
+++ python/website.py
@@ -2,8 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)      
 @app.route('/')
+def index():
+    return 'This is the index page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User is %s' % username
+
+@app.route('/home')
 def home():
-	return "Hey! This is my website."
+    return 'Hi this is my home page'
 
 if __name__ == '__main__':
 	app.run(debug=True)
