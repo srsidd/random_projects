3f08203d70b3b9941c34139e1c6111f408ac1a6a
srsidd <srsidd@users.noreply.github.com>
2019-11-20 10:20:04 -0500

    Delete install_script_dropbox_only.sh

--- bash/install_script_dropbox_only.sh
+++
@@ -1,4 +0,0 @@
-
-# Install dropbox
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-# ~/.dropbox-dist/dropboxd # Uncomment this for running dropbox and configuring with your account
